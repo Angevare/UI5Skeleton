1a9a27a7cdb804fb2204a3b9216c7030ffe27958
S0017146921 <patrick.angevare@delawareconsulting.com>
2018-12-05 14:54:10 +0000

    Add 'press' action to the list item and press handler in the view controller.

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -4,6 +4,16 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("SKE.UI5Skeleton.controller.Main", {
-
+		handleListItemPress: function (evt) {
+			// show in a pop-up which list element was pressed
+	      jQuery.sap.require("sap.m.MessageBox");
+			sap.m.MessageBox.show(
+				"You pressed item: " + evt.getSource().getBindingContext(), {
+					icon: sap.m.MessageBox.Icon.INFORMATION,
+					title: "function hanldeListItemPress",
+					actions: [sap.m.MessageBox.Action.OK]
+				}
+			);
+		}
 	});
 });
